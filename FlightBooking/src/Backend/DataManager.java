decf08ebd3665dfd16bf4563f24b24013ecb257c
Infinidrix <biruksolomon11@gmail.com>
2020-01-05 21:40:09 +0300

    Comments Added

--- FlightBooking/src/Backend/DataManager.java
+++ FlightBooking/src/Backend/DataManager.java
@@ -7,8 +7,17 @@ import java.sql.ResultSet;
 import java.util.HashMap;
 
 public interface DataManager {
+    // refresh the values of the object by syncing with database
     void sync();
+
+    // saves the changes in object to the database
     void update() throws InvalidEntry;
+
+    // returns a HashMap whose keys are the attributes and values are the
+    // values of those attributes
     HashMap<String, String> getAttributes();
+
+    // generate object by reading a row of the database
+    // doesn't move ResultSet to next entry
     void readRow(ResultSet row);
 }
